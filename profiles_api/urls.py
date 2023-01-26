a23b64e5570482b4cc998f2c179bc7c431bd203b
James <james.goss@outlook.com>
2023-01-26 21:35:34 +1000

    Added user profile feed API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,6 +6,7 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset') #register viewsets with the router
 router.register('profile', views.UserProfileViewSet) #base_name not required as base_name will be auto assigned to the name in the queryset (you could override it if you wanted by specifying it)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
